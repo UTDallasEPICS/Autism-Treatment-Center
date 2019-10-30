ac080be26bc10e31584bd14e35ed043d49a6e7bc
Byron Peng <blp180002@utdallas.edu>
2019-10-30 16:19:35 -0500

    Cleaned Up File Structure

---
+++ A/ATS/ATS/ATS.Android/obj/Debug/80/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 61961f66e1748b14f197c049c8cf469392940347 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1350
+	/* entry-length */
+	.word	264
+	/* value-offset */
+	.word	147
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 356401
+	.include	"typemap.mj.inc"
